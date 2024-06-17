b9a8e54cf3fda422c6b4b3b5fb2725410e4ab137
vortexdude <nitinnamdeo456@gmail.com>
2024-06-17 15:09:28 +0530

    :eyes: adding arg parser and docker, makefile and scripts

--- Viliar/entrypoint.sh
+++ Viliar/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-chmod +x ./run.sh
-
-bash $PACKAGE_DIR/script/run.sh >>/root/app.log 2>&1
-
-python3 $PACKAGE_DIR/app.py --port $PORT --host $HOST >>/root/app.log 2>&1
+python3 $PACKAGE_DIR/app.py --port $PORT --host $HOST --debug $dubug >>/app/app.log 2>&1
 
 exec "${@}"
