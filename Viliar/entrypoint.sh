6b8f7194543cc68f00e7579a94c272677ae55c13
vortexdude <nitinnamdeo456@gmail.com>
2024-06-24 18:04:55 +0530

    :heart: add new worker count parameter

--- Viliar/entrypoint.sh
+++ Viliar/entrypoint.sh
@@ -2,6 +2,7 @@
 
 cd $PACKAGE_DIR
 alembic upgrade head >> /app/db.log 2>&1
-python3 app.py --port=$PORT --host=$HOST --debug=$DEBUG >>/app/app.log 2>&1
+
+gunicorn 'app:app' -w ${WORKER_COUNT} -b ${HOST}:${PORT} >>/app/app.log 2>&1
 
 exec "${@}"
